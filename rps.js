0226f4a277b7aa17ec26cd211c38b55141905bcb
warmastr <jargyle80@yahoo.com>
2022-03-17 01:43:48 -0500

    Reworking game(), add functionality to playRound()

--- rps.js
+++ rps.js
@@ -1,4 +1,7 @@
 /* This program is intended to be played vs. the computer and has a random chance to win. First to five wins the game! */
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
     const compOptions = [0, 1, 2];
     let compChoice = Math.floor(Math.random() * compOptions.length);
@@ -35,22 +38,38 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay();
     let result;
     let weapons = playerSelection + computerSelection;
-    
+    let winner;
     if (weapons === 'rockpaper') {
         result = `Computer wins ${computerSelection} covers ${playerSelection}`;
+        winner = "computer";
     } else if (weapons === 'rockscissors') {
-        result = `Your ${playerSelection} beats the computers ${computerSelection}`;
+        result = `Your ${playerSelection} beats the computer's ${computerSelection}`;
+        winner = "player";
     } else if (weapons === 'paperrock') {
         result = `You win because ${playerSelection} covers ${computerSelection}`;
+        winner = "player";
     } else if (weapons === 'paperscissors') {
         result = `The computers' ${computerSelection} cut your ${playerSelection}.`;
+        winner = "computer";
     }else if (weapons === 'scissorsrock') {
         result = `You lose ${computerSelection} smashes your ${playerSelection}!`;
+        winner = "computer";
     }else if (weapons === 'scissorspaper') {
         result = `Winner Winner Chicken Dinner, ${playerSelection} cut ${computerSelection}!`;
+        winner = "player";
     } else {
         result = `It's a tie! You chose ${playerSelection}, the same as the computer! `;
+        winner = "tie";
     }
-    return result;
+    console.log(result);
+    return winner;
 }
-console.log(playRound(playerSelection, computerPlay));
+
+function game() {
+    let score;
+    for (let i = 0; i < 5; i++) {
+        playRound();
+
+    }
+
+game();
