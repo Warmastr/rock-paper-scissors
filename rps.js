4affda9e9d129d5f6b94f98961818c5b6f7450cb
warmastr <jargyle80@yahoo.com>
2022-03-17 21:10:15 -0500

    Finished game() & fixed return var in playRound()

--- rps.js
+++ rps.js
@@ -1,7 +1,4 @@
 /* This program is intended to be played vs. the computer and has a random chance to win. First to five wins the game! */
-let playerScore = 0;
-let computerScore = 0;
-
 function computerPlay() {
     const compOptions = [0, 1, 2];
     let compChoice = Math.floor(Math.random() * compOptions.length);
@@ -38,47 +35,58 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay();
     let result;
     let weapons = playerSelection + computerSelection;
-    let winner;
+    let roundWinner;
     if (weapons === 'rockpaper') {
         result = `Computer wins, ${computerSelection} covers ${playerSelection}`;
-        winner = "computer";
+        roundWinner = "computer";
     } else if (weapons === 'rockscissors') {
         result = `Your ${playerSelection} beats the computer's ${computerSelection}`;
-        winner = "player";
+        roundWinner = "player";
     } else if (weapons === 'paperrock') {
         result = `You win because ${playerSelection} covers ${computerSelection}`;
-        winner = "player";
+        roundWinner = "player";
     } else if (weapons === 'paperscissors') {
         result = `The computers' ${computerSelection} cut your ${playerSelection}.`;
-        winner = "computer";
+        roundWinner = "computer";
     }else if (weapons === 'scissorsrock') {
         result = `You lose ${computerSelection} smashes your ${playerSelection}!`;
-        winner = "computer";
+        roundWinner = "computer";
     }else if (weapons === 'scissorspaper') {
         result = `Winner Winner Chicken Dinner, ${playerSelection} cut ${computerSelection}!`;
-        winner = "player";
+        roundWinner = "player";
     } else {
         result = `It's a tie! You chose ${playerSelection}, the same as the computer! `;
-        winner = "tie";
+        roundWinner = "tie";
     }
     console.log(result);
-    return winner;
+    return roundWinner;
 }
-
 function game() {
-    for (let i = 0; i < 5; i++) { // play 5 rounds
-        playRound(playerSelection, computerPlay); // play a round
-        if (playRound() === 'player') {
-            playerScore += 1;
-            console.log(playerScore); // debug
-        } else if (playRound() === 'computer') {
-            computerScore += 1;
-            console.log(computerScore); // debug
-        } else {
-            computerScore += 0;
-            playerScore += 0; // basically do nothing, felt like I needed something in the else section for the tie game.
-        }
-        console.log(`You have ${playerScore} points. The computer has ${computerScore} points.`);
+    let playerScore = 0;
+    let computerScore = 0;
+        for (let i = 0; i < 5; i++) { // play 5 rounds
+            let winner = playRound(playerSelection, computerPlay); // play a round
+            if (winner === 'player') {
+                playerScore += 1;
+                console.log(playerScore); // debug
+            } else if (winner === 'computer') {
+                computerScore += 1;
+                console.log(computerScore); // debug
+            } else {
+                computerScore += 0;
+                playerScore += 0; // basically do nothing, felt like I needed something in the else section for the tie game.
+            }
+
+            if (playerScore > computerScore) {
+                gameWinner = 'You win, Great job!';
+            } else if (playerScore == computerScore) {
+                gameWinner = 'You tied the computer, try again!';
+            } else {
+                gameWinner = 'The computer whooped ya! Better luck next time!';
+            }
+            console.log(`You have ${playerScore} points. The computer has ${computerScore} points.`);
     }
+    console.log(gameWinner);
+    return gameWinner;
 }
 game();
